eb618863a90fa37079570f3f730a1ec3c947882f
Aniket Shahi <109075043+aniket0608@users.noreply.github.com>
2023-03-28 15:57:35 +0530

    Add files via upload

---
+++ MulCipher.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+string additiveCipher(string message, int key)
+{
+    string ciphertext = "";
+    for (int i = 0; i < message.length(); i++)
+    {  if(int(message[i]) >= 65 && int(message[i] <=90 )){
+        char currentchar = message[i];
+        char encryptedchar = ((currentchar - 'A') * key% 26) + 'A';
+        ciphertext += encryptedchar;
+    }
+       else{
+            ciphertext += message[i];
+        }
+    }
+    return ciphertext;
+}
+int main()
+{
+    string message;
+    int key;
+    cout << "Enter message: ";
+    getline(cin, message);
+    cout << "Enter key: ";
+    cin >> key;
+    string ciphertext = additiveCipher(message, key);
+    cout << "ciphertext: " << ciphertext << endl;
+    return 0;
+}
