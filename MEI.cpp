fa69457cc745b02c45616404d4b34ef895f309ea
aniket0608 <aniketshahi7766@gmail.com>
2023-04-14 22:33:17 +0530

    modified:   MEI.cpp

--- MEI.cpp
+++ MEI.cpp
@@ -1,5 +1,5 @@
 /*
-  C++ code for modular multiplicative 
+  C++ code for modular multiplicative
   inverse using an efficient approach of
   Extended Euclid Algorithm
 */
@@ -7,65 +7,65 @@
 using namespace std;
 
 // Function for extended Euclidean Algorithm
-int Find_gcd_Extended(int a, int b, int* x, int* y);
+int Find_gcd_Extended(int a, int b, int *x, int *y);
 
 // Function to find modulo inverse of a
 void Find_mod_Inverse(int a, int m)
 {
-    int x, y;
-    int g = Find_gcd_Extended(a, m, &x, &y);
-    
+  int x, y;
+  int g = Find_gcd_Extended(a, m, &x, &y);
+
+  /*
+    If the gcd value doesn't matches
+    to 1, then inverse doesn't exist
+  */
+  if (g != 1)
+  {
+    cout << "Inverse doesn't exist";
+  }
+  else
+  {
     /*
-      If the gcd value doesn't matches 
-      to 1, then inverse doesn't exist
+      m is added once more to handle
+      negative values
     */
-    if (g != 1)
-    {
-        cout << "Inverse doesn't exist";
-    }
-    else
-    {
-        /*
-          m is added once more to handle
-          negative values
-        */
-        int res = (x % m + m) % m;
-        cout<<res<<endl;
-    }
+    int res = (x % m + m) % m;
+    cout << res << endl;
+  }
 }
 
 // Function to find extended Euclidean Algorithm
-int Find_gcd_Extended(int a, int b, int* x, int* y)
+int Find_gcd_Extended(int a, int b, int *x, int *y)
 {
-    
-    // Base Case
-    if (a == 0)
-    {
-        *x = 0, *y = 1;
-        return b;
-    }
-    
-    // Storing results of recursive call
-    int x1, y1;
-    int gcd = Find_gcd_Extended(b % a, a, &x1, &y1);
 
-    /*
-      After recursive call, update the values
-      of x and y
-    */
-    *x = y1 - (b / a) * x1;
-    *y = x1;
+  // Base Case
+  if (a == 0)
+  {
+    *x = 0, *y = 1;
+    return b;
+  }
 
-    return gcd;
+  // Storing results of recursive call
+  int x1, y1;
+  int gcd = Find_gcd_Extended(b % a, a, &x1, &y1);
+
+  /*
+    After recursive call, update the values
+    of x and y
+  */
+  *x = y1 - (b / a) * x1;
+  *y = x1;
+
+  return gcd;
 }
 
 // Driver Code
 int main()
 {
-    int a = 5, m = 7;
+  int a = 5, m = 7;
 
-    // Function call
-    cout<<"The value of x is: ";
-    Find_mod_Inverse(a, m);
-    return 0;
+  // Function call
+  cout << "The value of x is: ";
+  Find_mod_Inverse(a, m);
+  return 0;
 }
