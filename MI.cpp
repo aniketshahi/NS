bd63cc8fe99323d05edd0f0e8e228f4a601f5b48
aniket0608 <aniketshahi7766@gmail.com>
2023-04-15 20:16:42 +0530

    modified:   MulCipher.cpp

--- MI.cpp
+++ MI.cpp
@@ -15,12 +15,14 @@ int main()
         int inverse = 0;
         for (int j = 1; j < MODULUS; j++)
         {
-            if ((i * j) % MODULUS == 1) {
+            if ((i * j) % MODULUS == 1)
+            {
                 inverse = j;
                 break;
             }
         }
-        if (inverse > 0 && inverse != i) {
+        if (inverse > 0 && inverse != i)
+        {
             cout << i << " and " << inverse << endl;
         }
     }
