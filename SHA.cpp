84b81e7dd401f4a11023bf0d5309c091f3456a6b
aniket0608 <aniketshahi7766@gmail.com>
2023-04-17 22:54:46 +0530

    modified:   SHA.cpp

--- SHA.cpp
+++ SHA.cpp
@@ -6,7 +6,7 @@
 std::string sha256(const std::string &str)
 {
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
+    S sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, str.c_str(), str.size());
     SHA256_Final(hash, &sha256);
